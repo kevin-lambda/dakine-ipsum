e46aab78d5f8ee5e8111a5b4f426951dac552256
Kevin Lam <mtn013209@gmail.com>
2023-01-18 16:02:36 -0800

    atest

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutPage />}></Route>
-        <Route path="/test" element={<p>this is the test page</p>}></Route>
       </Routes>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter as Router } from "react-router-dom"
+// import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   // <Router basename="/dakine-ipsum">
-  <Router basename={process.env.PUBLIC_URL}>
-    {/* <Router basename={window.location.pathname || ""}> */}
+  //   <Router basename={window.location.pathname || ""}>
+  // <Router basename={process.env.PUBLIC_URL}>
+  <Router basename="/dakine-ipsum">
     <App />
   </Router>
 )
